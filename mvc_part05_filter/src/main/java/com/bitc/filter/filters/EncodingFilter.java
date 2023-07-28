580b558413a7e3e1f8827e8cfb27377a0f8d23c1
m1njunK <rla6085@naver.com>
2023-07-28 13:48:05 +0900

    update

--- mvc_part05_filter/src/main/java/com/bitc/filter/filters/EncodingFilter.java
+++ mvc_part05_filter/src/main/java/com/bitc/filter/filters/EncodingFilter.java
@@ -8,14 +8,17 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 public class EncodingFilter implements Filter{
 
+	private String encoding;
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		System.out.println(" init EncodingFilter Start ");
-		
-		System.out.println(" init EncodingFilter End ");
+		encoding = filterConfig.getInitParameter("encoding");
+		System.out.println(" init EncodingFilter End : " + encoding);
 	}
 
 	@Override
@@ -23,7 +26,14 @@ public class EncodingFilter implements Filter{
 			throws IOException, ServletException {
 		System.out.println(" doFilter EncodingFilter Start ");
 		// 전처리
-		request.setCharacterEncoding("UTF-8");
+		HttpServletRequest req = (HttpServletRequest) request;
+		String method = req.getMethod();
+		
+		if(method.toUpperCase().equals("POST")) {
+			request.setCharacterEncoding(encoding);
+		}
+		// FilterChain에 등록된 다른 Filter의 doFilter 호출
+		// FilterChain의 모든 doFilter method가 호출 되면 Servlet으로 요청 전달 후 결과 확인
 		chain.doFilter(request, response);
 		// 후처리
 		System.out.println(" doFilter EncodingFilter End ");
--- mvc_part05_filter/src/main/webapp/WEB-INF/views/home.jsp
+++ mvc_part05_filter/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,3 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" pageEncoding="UTF-8"%>
 <html>
 <head>
