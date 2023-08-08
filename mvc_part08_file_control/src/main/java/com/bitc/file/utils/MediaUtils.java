6b2c0c306e4b7c1abdd61c0570d13bfcadd8aeb0
m1njunK <rla6085@naver.com>
2023-08-08 14:37:29 +0900

    update

---
+++ mvc_part08_file_control/src/main/java/com/bitc/file/utils/MediaUtils.java
@@ -0,0 +1,27 @@
+package com.bitc.file.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.MediaType;
+
+/**
+ * 확장자를 이용하여
+ * Image File type을 MediaType으로 구분할 객체
+ */
+public class MediaUtils {
+	
+	private static Map<String, MediaType> mediaMap;
+	
+	static {
+		mediaMap = new HashMap<>();
+		mediaMap.put("JPG",MediaType.IMAGE_JPEG);		//image/jpeg
+		mediaMap.put("JPEG",MediaType.IMAGE_JPEG);		//image/jpeg
+		mediaMap.put("GIF",MediaType.IMAGE_GIF);		//image/gif
+		mediaMap.put("PNG",MediaType.IMAGE_PNG);		//image/png
+	}
+	
+	public static MediaType getMediaType(String ext) {
+		return mediaMap.get(ext.toUpperCase());
+	}
+}
